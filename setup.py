feb69acc65fb0b6a98f5b494aac0515a62d0c797
Ben McGinnes <ben@adversary.org>
2018-02-12 12:07:26 +1100

    Parsing fix
    * Fixed some incorrectparenthesis and options, including restoring
      punycoder name.
    * Suspect v2 version might need renaming so original module name can
      be specified by way of an "import as" method.

--- setup.py
+++ setup.py
@@ -18,24 +18,24 @@ if sys.version_info[0] == 2:
             scripts=['punycodeurl/__init__.py']
             )
 elif sys.version_info[0] == 3:
-    setup(name='punycodeurl',
-          # name='punycoder',
+    setup(name='punycoder',
           version='0.2',
           description='punycoder for URL',
           author='Ben McGinnes',
           author_email='ben@adversary.org',
           url='https://github.com/Hasimir/punycodeurl',
-        platforms='any',
-        install_requires=[],
+          platforms='any',
+          install_requires=[],
             packages=['punycoder'],
-        classifiers=[
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: Apache LICENSE 2.0',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-            'Topic :: Internet',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            scripts=['punycoder/__init__.py']
-            )
+            scripts=['punycoder/__init__.py'],
+            classifiers=[
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: Apache LICENSE 2.0',
+                'Topic :: Software Development :: Libraries :: Python Modules',
+                'Topic :: Internet',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+                'Programming Language :: Python :: 3.6'
+                ])
+                
