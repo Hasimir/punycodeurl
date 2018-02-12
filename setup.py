80a99d6c624d6431480ec3aee6861d6c2c418208
Ben McGinnes <ben@adversary.org>
2018-02-12 11:06:11 +1100

    New setup file
    * Non executable setup file which combines setup.py2 and 3 with very
      minor changes.
    * Intended to be run with the intended python version manually, it
      then determines the module to install based on the sys.version_info.
    * py3 version name modified to match the original, but no other
      settings changed (yet).

---
+++ setup.py
@@ -0,0 +1,41 @@
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.version_info[0] == 2:
+    setup(name='punycodeurl',
+          version='0.1',
+          description='punycodeurl for Url',
+          author='DaeMyung Kang',
+          author_email='charsyam@gmail.com',
+          url='https://github.com/charsyam/punycodeurl',
+          platforms='any',
+            install_requires=[],
+            packages=['punycodeurl'],
+            scripts=['punycodeurl/__init__.py']
+            )
+elif sys.version_info[0] == 3:
+    setup(name='punycodeurl',
+          # name='punycoder',
+          version='0.2',
+          description='punycoder for URL',
+          author='Ben McGinnes',
+          author_email='ben@adversary.org',
+          url='https://github.com/Hasimir/punycodeurl',
+        platforms='any',
+        install_requires=[],
+            packages=['punycoder'],
+        classifiers=[
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache LICENSE 2.0',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Internet',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            scripts=['punycoder/__init__.py']
+            )
