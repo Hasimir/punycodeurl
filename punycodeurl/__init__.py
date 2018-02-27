d474186413a7262c40191ae7554c1a502e56c7b3
Ben McGinnes <ben@adversary.org>
2018-02-28 10:23:32 +1100

    Absolutist
    * Use absolute import for importing the relevant local files.

--- punycodeurl/__init__.py
+++ punycodeurl/__init__.py
@@ -1,11 +1,12 @@
 # -*- coding:utf-8 -*-
 
+import absolute_import
 import sys
 
 if sys.version_info[0] == 2:
-    from punycodeurl2 import *
+    from .punycodeurl2 import *
 elif sys.version_info[0] >= 3:
-    from punycodeurl3 import *
+    from .punycodeurl3 import *
 else:
-    print("The twentieth century called, it wants its code back.")
+    print("Check your Python version.")
     pass
